0028c131b3549e3ab64363ecb1c44ed497086b8e
yougrianes <398794706@qq.com>
2025-04-17 22:15:41 +0800

    qcnet code learning

--- 20250417.md
+++ 20250417.md
@@ -1,1 +1,1 @@
-![alt text](image-6.png)
+![alt text](image-7.png)
--- llmtest.py
+++ llmtest.py
@@ -1,10 +1,10 @@
 import ollama
 
 response = ollama.chat(
-    model='llama3.2-vision',
+    model='llama3.2-vision:11b',
     messages=[{
         'role': 'user',
-        'content': 'What is in this image?',
+        'content': '这张图片描述了什么',
         'images': ['/media/li-ruiqin/5D4A4FF551440BFB/workplace/notes/image/image-15.png']
     }]
 )
